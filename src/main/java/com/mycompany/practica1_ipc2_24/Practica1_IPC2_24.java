b99f5dd6e774516e77417659f57e692e515febb0
Kenny <kennylopez202031554@cunoc.edu.gt>
2024-02-27 19:02:47 -0600

    primer commit

---
+++ src/main/java/com/mycompany/practica1_ipc2_24/Practica1_IPC2_24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1_ipc2_24;
+
+/**
+ *
+ * @author Kenny Salazar
+ */
+public class Practica1_IPC2_24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
